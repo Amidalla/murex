04570ffd3093a638ebd254dfd8f9c98edf214535
alegawra <alegawra@yandex.ru>
2026-03-28 16:24:47 +0300

    first

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -4,6 +4,7 @@ import "../styles/header.scss";
 import "../styles/footer.scss";
 import "../styles/home.scss";
 import "../styles/modals.scss";
+import "../styles/catalog.scss";
 import LazyLoad from "vanilla-lazyload";
 import { Fancybox } from "@fancyapps/ui";
 import "@fancyapps/ui/dist/fancybox/fancybox.css";
